cbb296d91545f3fc3cd67581269e767004aa4f7d
paprikun <32676078+paprikun@users.noreply.github.com>
2017-10-12 17:31:12 +0400

    Update myautotest.js

--- myautotest.js
+++ myautotest.js
@@ -3,8 +3,6 @@ let driver = new Builder()
     .forBrowser('chrome')
     .build();
 driver.get('http://www.google.com/ncr');
-var currentURL = driver.getCurrentUrl().toString();
-console.log("***>>> getCurrentUrl: "+currentURL);
 driver.findElement(By.name('q')).sendKeys('webdriver');
 driver.findElement(By.name('btnG')).click();
 driver.wait(until.titleIs('webdriver - Google Search'), 1000);
